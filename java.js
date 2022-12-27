d927885224fea047adf64e3558b45ab5aaf4c275
Ahmed Ali <mughala774@gmail.com>
2022-12-27 22:04:35 +0500

    my website

--- java.js
+++ java.js
@@ -31,43 +31,50 @@
 // }
 
 
-
-
-
-
-
-
-    const Res01 = document.getElementById("select");
-    const Res02 = document.getElementById("select2");
-
-    const Name01 = document.getElementById("name");
-    const Name02 = document.getElementById("name2");
-
-     
-     
-
-
-
    function table(){
-
-    
     const Res01 = document.getElementById("select").value;
     const Res02 = document.getElementById("select2").value;
 
     const Name01 = document.getElementById("name");
     const Name02 = document.getElementById("name2");
-    // alert(Res01);
-    // alert(Res02);
-//    console.log(Res01);
+    
 
+    // console.log(Name01);
+    // console.log(Name02);
     if(Res01 > Res02){
-        alert("Hallo world 5");
+        // alert("Hallo world 5");
+        document.getElementById("final1").innerHTML = Name01.value;
     }
     else if(Res01 < Res02) {
-        alert("Hallo World 10");
+        // alert("Hallo World 10");
+        document.getElementById("final1").innerHTML = Name02.value;
     }
     else{
-        alert("Ali");
+        document.getElementById("final1").innerHTML = "";
+    }
+
+}  
+
+function table2(){
+    const Res03 = document.getElementById("select3").value;
+    const Res04 = document.getElementById("select4").value;
+
+    const Name03 = document.getElementById("name3");
+    const Name04 = document.getElementById("name4");
+    
+
+    // console.log(Name01);
+    // console.log(Name02);
+    if(Res03 > Res04){
+        // alert("Hallo world 5");
+        document.getElementById("final2").innerHTML = Name03.value;
+    }
+    else if(Res03 < Res04) {
+        // alert("Hallo World 10");
+        document.getElementById("final2").innerHTML = Name04.value;
+    }
+    else{
+        document.getElementById("final2").innerHTML = "";
     }
 
 }  
@@ -75,8 +82,178 @@
 
 
 
+function table3(){
+    const Res05 = document.getElementById("select5").value;
+    const Res06 = document.getElementById("select6").value;
+
+    const Name05 = document.getElementById("name5");
+    const Name06 = document.getElementById("name6");
+    
+
+    // console.log(Name01);
+    // console.log(Name02);
+    if(Res05 > Res06){
+        // alert("Hallo world 5");
+        document.getElementById("final3").innerHTML = Name05.value;
+    }
+    else if(Res05 < Res06) {
+        // alert("Hallo World 10");
+        document.getElementById("final3").innerHTML = Name06.value;
+    }
+    else{
+        document.getElementById("final3").innerHTML = "";
+    }
+
+}  
+function table4(){
+    const Res07 = document.getElementById("select7").value;
+    const Res08 = document.getElementById("select8").value;
+
+    const Name07 = document.getElementById("name7");
+    const Name08 = document.getElementById("name8");
+    
+    // console.log(Name01);
+    // console.log(Name02);
+    if(Res07 > Res08){
+        // alert("Hallo world 5");
+        document.getElementById("final4").innerHTML = Name07.value;
+    }
+    else if(Res07 < Res08) {
+        // alert("Hallo World 10");
+        document.getElementById("final4").innerHTML = Name08.value;
+    }
+    else{
+        document.getElementById("final4").innerHTML = "";
+    }
+
+}  
 
 
+
+// Sami Final
+
+// function table5(){
+//     const Res09 = document.getElementById("select9").value;
+//     const Res10 = document.getElementById("select10").value;
+
+//     var Name09 = document.getElementById("final1");
+//     var Name10 = document.getElementById("final2");
+    
+
+//     // console.log(Name10);
+//     // console.log(Name09);
+//     // console.log(Res10);
+//     if(Res09 > Res10){
+//         // alert("Hallo world 5");
+//         document.getElementById("final5").innerHTML =Name09;
+//     }
+//     else if(Res09 < Res10) {
+//         // alert("Hallo World 10");
+//         document.getElementById("final5").innerHTML =Name10;
+//     }
+//     else{
+//         // alert("Ali")
+//         document.getElementById("final5").innerHTML = "";
+//     }
+
+// }  
+
+
+
+function table5(){
+    const Res09 = document.getElementById("select9").value;
+    const Res10 = document.getElementById("select10").value;
+
+    const Name09 = document.getElementById("final1").textContent;
+    const Name10 = document.getElementById("final2").textContent;
+    
+
+    // console.log(Name10);
+    // console.log(Name09);
+    // console.log(Res10);
+    if(Res09 > Res10){
+        // alert("Hallo world 5");
+        document.getElementById("final5").innerHTML = Name09;
+        // document.getElementById("final1").textContents;
+        // alert(Name09);
+    }
+    else if(Res09 < Res10) {
+        // alert("Hallo World 10");
+        document.getElementById("final5").innerHTML = Name10;
+        // document.getElementById("final2").innerHTML;
+        // alert(Name10);
+    }
+    else{
+        // alert("Ali")
+        document.getElementById("final5").innerHTML = "";
+    }
+
+}  
+
+function table6(){
+    const Res11 = document.getElementById("select11").value;
+    const Res12 = document.getElementById("select12").value;
+
+    const Name11 = document.getElementById("final3").textContent;
+    const Name12 = document.getElementById("final4").textContent;
+    
+
+    // console.log(Name10);
+    // console.log(Name09);
+    // console.log(Res10);
+    if(Res11 > Res12){
+        // alert("Hallo world 5");
+        document.getElementById("final6").innerHTML = Name11;
+        // document.getElementById("final1").textContents;
+        // alert(Name09);
+    }
+    else if(Res11 < Res12) {
+        // alert("Hallo World 10");
+        document.getElementById("final6").innerHTML = Name12;
+        // document.getElementById("final2").innerHTML;
+        // alert(Name10);
+    }
+    else{
+        // alert("Ali")
+        document.getElementById("final6").innerHTML = "";
+    }
+
+}  
+
+
+
+// FINAL
+
+
+function table7(){
+    const Res13 = document.getElementById("select13").value;
+    const Res14 = document.getElementById("select14").value;
+
+    const Name13 = document.getElementById("final5").textContent;
+    const Name14 = document.getElementById("final6").textContent;
+    
+
+    // console.log(Name10);
+    // console.log(Name09);
+    // console.log(Res10);
+    if(Res13 > Res14){
+        // alert("Hallo world 5");
+        document.getElementById("final7").innerHTML = Name13;
+        // document.getElementById("final1").textContents;
+        // alert(Name09);
+    }
+    else if(Res13 < Res14) {
+        // alert("Hallo World 10");
+        document.getElementById("final7").innerHTML = Name14;
+        // document.getElementById("final2").innerHTML;
+        // alert(Name10);
+    }
+    else{
+        // alert("Ali")
+        document.getElementById("final7").innerHTML = "";
+    }
+
+}  
 
 
 
